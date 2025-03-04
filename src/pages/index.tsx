a703405562f3792b25c594a826457a2481ad84c1
Nam Dao <n.dao@f5.com>
2025-03-04 06:16:00 -0800

    monaco editor working, and assets are in place to mimic app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,28 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
-import Editor from "@monaco-editor/react";
+import Editor, { Monaco } from "@monaco-editor/react";
 import { Layout } from "@emerald/react";
 import styled from "styled-components";
-import { configureMonacoYaml} from "monaco-yaml";
+import { configureMonacoYaml } from "monaco-yaml";
 import configmap from "./configmap.png";
+import gheader from "./sideheader.png";
+import yamlWorker from "../../node_modules/monaco-yaml/yaml.worker.js";
 
 // @ts-ignore
 window.MonacoEnvironment = {
   getWorker(moduleId, label) {
-    switch (label) {
-      case 'editorWorkerService':
-        return new Worker(new URL('monaco-editor/esm/vs/editor/editor.worker', import.meta.url))
-      case 'css':
-      case 'less':
-      case 'scss':
-        return new Worker(new URL('monaco-editor/esm/vs/language/css/css.worker', import.meta.url))
-      case 'handlebars':
-      case 'html':
-      case 'razor':
-        return new Worker(
-          new URL('monaco-editor/esm/vs/language/html/html.worker', import.meta.url)
-        )
-      case 'json':
-        return new Worker(
-          new URL('monaco-editor/esm/vs/language/json/json.worker', import.meta.url)
-        )
-      case 'javascript':
-      case 'typescript':
-        return new Worker(
-          new URL('monaco-editor/esm/vs/language/typescript/ts.worker', import.meta.url)
-        )
-      case 'yaml':
-        return new Worker(new URL('monaco-yaml/yaml.worker', import.meta.url))
-      default:
-        throw new Error(`Unknown label ${label}`)
-    }
-  }
-}
+      if (label === "yaml") {
+       console.log(import.meta.url, "hello word", 'line 15')
+
+        return new Worker("../../node_modules/monaco-yaml/yaml.worker.js");
+      }
+  },
+};
 const IndexPage: React.FC<PageProps> = () => {
-  const handleMount = (monaco) => {
+  const handleMount = (monaco: Monaco) => {
     if (monaco) {
       configureMonacoYaml(monaco, {
-        enableSchemaRequest: true,
+        enableSchemaRequest: false,
         schemas: [
           {
             // If YAML file is opened matching this glob
@@ -53,7 +33,7 @@ const IndexPage: React.FC<PageProps> = () => {
               properties: {
                 name: {
                   type: "string",
-                  description: 9999,
+                  description: "awesome",
                 },
                 age: {
                   type: "integer",
@@ -64,23 +44,31 @@ const IndexPage: React.FC<PageProps> = () => {
                 },
               },
             },
-            uri: "https://github.com/remcohaszing/monaco-yaml#usage",
+            uri: "https://google.com",
           },
         ],
       });
+      console.log(monaco.Uri.parse("file:///person.yaml"));
 
-     const model =  monaco.editor.createModel(
-        "name: John Doe\nage: ee\noccupation: Pirate\n",
+      monaco.editor.createModel(
+        'name: John Doe\nage: 42\noccupation: Pirate\n',
         undefined,
-        monaco.Uri.parse("file://person.yaml")
-      );
+        monaco.Uri.parse('file:///person.yaml')
+      )
     }
   };
 
   return (
     <Layout
       sidebar={
-       <Editor value={"123"} language={"yaml"} beforeMount={handleMount} path={"person.yaml"}></Editor>
+        <div style={{ marginTop: "-30px", height: "100%" }}>
+          <Editor
+            value={"123"}
+            language={"yaml"}
+            beforeMount={handleMount}
+            path={"person.yaml"}
+          ></Editor>
+        </div>
       }
       sidebarWidth={700}
       padSidebarVertical={20}
@@ -88,7 +76,9 @@ const IndexPage: React.FC<PageProps> = () => {
       padWorkspaceVertical={20}
       padWorkspaceHorizontal={20}
       sidebarHeader={
-        <div style={{ height: "100%" }}>F5 AI Gate Way Config Editor</div>
+        <div style={{ height: "100%" }}>
+          <img src={gheader} style={{ height: "100%", width: "100%" }} />
+        </div>
       }
       footer={<span>Footer</span>}
       themeOverride={{
@@ -102,7 +92,9 @@ const IndexPage: React.FC<PageProps> = () => {
         },
       }}
     >
-      <div style={{ height: "100%", paddingTop: "60px" }}><img src={configmap} height={"80%"} width={"100%"}></img></div>
+      <div style={{ height: "100%", paddingTop: "60px" }}>
+        <img src={configmap} height={"80%"} width={"100%"}></img>
+      </div>
     </Layout>
   );
 };
